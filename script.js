d13deb48c86f32687850a53b1ff0bccea6acfc53
Dickens-Vida <gabrielozanosan@gmail.com>
2026-07-05 22:29:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ if (boton) {
         });
     });
 }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".section").forEach((el) => observer.observe(el));
